9f8205e1ec4ffb6e04240aec3d957ae50eb30fd9
202166manoj <153445945+202166manoj@users.noreply.github.com>
2024-11-05 22:44:51 +0530

    Create purchase_report.php

---
+++ purchase_report.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Purchase Report</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="shortcut icon" href="images/icon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/sidenav.css">
+    <link rel="stylesheet" href="css/home.css">
+    <script src="js/report.js"></script>
+    <script src="js/restrict.js"></script>
+    <style>
+        .back-btn {
+            background-color: white;
+            color:  #28a745;
+            border: none;
+            padding: 5px 10px;
+            border-radius: 5px;
+            font-size: 16px;
+            cursor: pointer;
+            margin-bottom: 20px;
+        }
+        .back-btn:hover {
+            background-color: #e2e6ea;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container-fluid">
+        <div class="container">
+
+            <!-- Back to Dashboard Button -->
+            <button class="back-btn" onclick="window.location.href='home.php';"><i class="fa fa-arrow-left"></i> Back to Dashboard</button>
+
+            <!-- header section -->
+            <?php
+                require "php/header.php";
+                createHeader('book', 'Purchase Report', 'Showing Purchase Report');
+            ?>
+            <!-- header section end -->
+
+            <!-- form content -->
+            <div class="row">
+
+                <div class="col-md-12 form-group form-inline">
+                    <label class="font-weight-bold" for="">Start Date :&emsp;</label>
+                    <input type="date" class="form-control" id="start_date" onchange="showReport('purchase');">
+                    &emsp;
+                    <label class="font-weight-bold" for="">End Date :&emsp;</label>
+                    <input type="date" class="form-control" id="end_date" onchange="showReport('purchase');">
+                    &emsp;
+                    <button class="btn btn-success" onclick="location.reload();"><i class="fa fa-refresh"></i></button>
+                </div>
+
+                <div class="col col-md-12">
+                    <hr class="col-md-12" style="padding: 0px; border-top: 2px solid #28a745;">
+                </div>
+
+                <div class="col col-md-12 table-responsive">
+                    <div id="print_content" class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover" id="purchase_report_div">
+                            <?php
+                            require "php/report.php";
+                            showPurchases("", "");
+                            ?>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="col-md-12 text-center">
+                    <button class="btn btn-primary" onclick="printReport('Purchase');">Print</button>
+                </div>
+
+            </div>
+            <!-- form content end -->
+            <hr style="border-top: 2px solid #ff5252;">
+        </div>
+    </div>
+</body>
+</html>
